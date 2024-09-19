1e1d276ae23637c61ba6648ef903182a19d2d40a
rupali <rupalibodge97@gmail.com>
2024-09-19 17:44:19 +0530

    Food changes

---
+++ src/assets/assets.js
@@ -0,0 +1,17 @@
+import logo from './logo.png'
+import add_icon_white from './add_icon_white.png'
+import profile_icon from './profile_icon.png'
+import parcel_icon from './parcel_icon.png'
+import ordericon1 from './ordericon1.jpg'
+import upload from './upload.jpg'
+export const assets = {
+    logo,
+    upload,
+    add_icon_white,
+    profile_icon,
+    parcel_icon,
+    ordericon1,
+    
+
+}
+
